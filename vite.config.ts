ee2501c4f998d2249e522a580c2357fcc9bf973d
Jake Hong <jakehong0521@berkeley.edu>
2024-07-09 23:49:37 -0700

    Use pnpm for build on github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   assetsInclude: ["src/static/*"],
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 5173,
+  },
 })
